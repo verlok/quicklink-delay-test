f5f60a2f9b9c58c0f878ee26aa92c339655f86e8
Andrea Verlicchi <andrea.verlicchi@ynap.com>
2021-06-11 18:32:59 +0200

    Second version throttle using npm throttles

--- js/page.js
+++ js/page.js
@@ -1,69 +1,60 @@
-{
-    const queue = [];
-    const fakePrefetchTime = 1000; //TODO: make a config
+import throttles from "../node_modules/throttles/dist/index.mjs";
 
-    let throttle,
-        delay,
-        countPrefetching = 0,
-        countPrefetched = 0;
+const $button = document.querySelector("button");
+const $delay = document.querySelector("#delay");
+const $throttle = document.querySelector("#throttle");
+const $prefetching = document.querySelector("#prefetching");
+const $prefetched = document.querySelector("#prefetched");
 
-    const $button = document.querySelector("button");
-    const $delay = document.querySelector("#delay");
-    const $throttle = document.querySelector("#throttle");
-    const $prefetching = document.querySelector("#prefetching");
-    const $prefetched = document.querySelector("#prefetched");
+const fakePrefetchTime = 1000; //TODO: make this a config
 
-    const fakePrefetch = (el) => {
-        countPrefetching += 1;
+let delay,
+    countPrefetching = 0,
+    countPrefetched = 0,
+    toAdd,
+    isDone;
+
+const fakePrefetch = (el) => {
+    countPrefetching += 1;
+    $prefetching.innerHTML = countPrefetching;
+    el.classList.add("fetch-started");
+    setTimeout(() => {
+        countPrefetching -= 1;
         $prefetching.innerHTML = countPrefetching;
-        el.classList.add("fetch-started");
-        setTimeout(() => {
-            countPrefetching -= 1;
-            $prefetching.innerHTML = countPrefetching;
-            countPrefetched += 1;
-            $prefetched.innerHTML = countPrefetched;
-            el.classList.add("fetch-completed");
-            manageQueue();
-        }, fakePrefetchTime);
-    };
+        countPrefetched += 1;
+        $prefetched.innerHTML = countPrefetched;
+        el.classList.add("fetch-completed");
+        isDone();
+    }, fakePrefetchTime);
+};
 
-    const manageQueue = () => {
-        for (let i = 0; i < queue.length; i += 1) {
-            if (countPrefetching >= throttle) return;
-            const el = queue.shift();
+const onenter = (el) => {
+    const timer = setTimeout(() => {
+        toAdd(() => {
             fakePrefetch(el);
-        }
-    };
-
-    const enqueuePrefetch = (el) => {
-        queue.push(el);
-        manageQueue();
-    };
-    const onenter = (el) => {
-        const timer = setTimeout(() => {
-            enqueuePrefetch(el);
-        }, parseInt(delay));
-        el.setAttribute("data-timer", timer);
-    };
-    const onexit = (el) => {
-        const timer = el.getAttribute("data-timer");
-        if (!timer) return;
-        clearTimeout(timer);
-        el.removeAttribute("data-timer");
-        el.classList.remove("fetch-started");
-    };
-
-    const start = () => {
-        $button.disabled = true;
-        delay = $delay.value;
-        throttle = $throttle.value;
-        new LazyLoad({
-            elements_selector: "article",
-            callback_enter: onenter,
-            callback_exit: onexit,
-            cancel_on_exit: false,
-            threshold: 0
         });
-    };
-    $button.addEventListener("click", start);
-}
+    }, parseInt(delay));
+    el.setAttribute("data-timer", timer);
+};
+
+const onexit = (el) => {
+    const timer = el.getAttribute("data-timer");
+    if (!timer) return;
+    clearTimeout(timer);
+    el.removeAttribute("data-timer");
+    el.classList.remove("fetch-started");
+};
+
+const start = () => {
+    $button.disabled = true;
+    delay = $delay.value;
+    [toAdd, isDone] = throttles($throttle.value || 1 / 0);
+    new LazyLoad({
+        elements_selector: "article",
+        callback_enter: onenter,
+        callback_exit: onexit,
+        cancel_on_exit: false,
+        threshold: 0
+    });
+};
+$button.addEventListener("click", start);
